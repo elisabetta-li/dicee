c5bfe6bf2ba34a5d2219ec4830d2cefbb53de4cd
Elizaveta <kashirskayaelizaveta123@gmail.com>
2021-02-08 21:19:23 +0300

    Add DicePage and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+
+    return Center(
+    );
   }
 }
+
